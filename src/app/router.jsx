be7715dc48b6db4a5fed81199195c9a14a7974db
Devinchia101004 <devinchia8@gmail.com>
2026-03-03 11:10:33 +0700

    Initial project setup with routing structure

---
+++ src/app/router.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+
+import Home from "../pages/Home/Home";
+import AddProject from "../pages/AddProject/AddProject";
+import Customize from "../pages/Customize/Customize";
+import MyWorks from "../pages/MyWorks/MyWorks";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-project" element={<AddProject />} />
+          <Route path="/customize/:title" element={<Customize />} />
+          <Route path="/my-works" element={<MyWorks />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Customize/Customize.jsx
@@ -0,0 +1,7 @@
+import { useParams } from "react-router-dom";
+
+export default function Customize() {
+  const { title } = useParams();
+
+  return <h1>Customize Page: {title}</h1>;
+}
